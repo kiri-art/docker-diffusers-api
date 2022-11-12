ae4cdaaf3cc2dab2db2db7bf83e3f0bdd503a122
Gadi Cohen <dragon@wastelands.net>
2022-11-12 07:09:08 +0000

    getting started

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ import distutils
 from uuid import uuid4
 from io import BytesIO
 from PIL import Image
-from pathlib import Path
+from pathlib import Path, PosixPath
 
 path = os.path.dirname(os.path.realpath(sys.argv[0]))
 TESTS = path + os.sep + "tests"
@@ -22,8 +22,13 @@ Path(OUTPUT).mkdir(parents=True, exist_ok=True)
 
 
 def b64encode_file(filename: str):
-    with open(os.path.join(FIXTURES, filename), "rb") as file:
-        return base64.b64encode(file.read())
+    path = (
+        filename
+        if isinstance(filename, PosixPath)
+        else os.path.join(FIXTURES, filename)
+    )
+    with open(path, "rb") as file:
+        return base64.b64encode(file.read()).decode("ascii")
 
 
 def output_path(filename: str):
@@ -224,6 +229,7 @@ def main(tests_to_run, banana, extraCallInputs):
         runTest(test, banana, extraCallInputs)
 
 
+"""
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--banana", required=False, action="store_true")
@@ -254,3 +260,45 @@ if __name__ == "__main__":
         tests_to_run = list(all_tests.keys())
 
     main(tests_to_run, banana=args.banana, extraCallInputs=extraCallInputs)
+"""
+
+if True or os.getenv("USE_DREAMBOOTH"):
+    name = "dreambooth"
+    inputs = {
+        "modelInputs": {
+            "prompt": "girl with a pearl earing standing in a big room",
+            "instance_images": list(
+                map(b64encode_file, list(Path("tests/fixtures/dreambooth").iterdir()))
+            ),
+            # "init_image": b64encode_file("girl_with_pearl_earing_outpainting_in.png"),
+        },
+        "callInputs": {
+            "MODEL_ID": "runwayml/stable-diffusion-v1-5",
+            "PIPELINE": "StableDiffusionPipeline",
+            "SCHEDULER": "LMSDiscreteScheduler",
+        },
+    }
+    print(inputs)
+
+    # print(json.dumps(inputs, indent=4))
+    exit
+
+    """
+    print("Running test: " + name)
+    response = requests.post("http://localhost:8000/", json=inputs)
+    result = response.json()
+
+    if result.get("images_base64", None) == "None":
+        print(json.dumps(result, indent=4))
+        print()
+        exit
+
+    images_base64 = result.get("images_base64", None)
+    if images_base64:
+        for idx, image_byte_string in enumerate(images_base64):
+            decode_and_save(image_byte_string, f"{name}_{idx}")
+    else:
+        decode_and_save(result["image_base64"], name)
+
+    print()
+    """
