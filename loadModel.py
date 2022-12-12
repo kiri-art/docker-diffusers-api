c2cb368bb398ecae91a5625a6d298ceb35514413
Gadi Cohen <dragon@wastelands.net>
2022-12-12 07:22:38 +0000

    chore(loadModel): show loading timings

--- loadModel.py
+++ loadModel.py
@@ -3,6 +3,7 @@ import os
 from diffusers import pipelines as _pipelines, StableDiffusionPipeline
 from getScheduler import getScheduler, DEFAULT_SCHEDULER
 from precision import revision, torch_dtype
+import time
 
 HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
 PIPELINE = os.getenv("PIPELINE")
@@ -29,6 +30,7 @@ def loadModel(model_id: str, load=True):
 
     scheduler = getScheduler(model_id, DEFAULT_SCHEDULER, not load)
 
+    from_pretrained = time.time()
     model = pipeline.from_pretrained(
         model_id,
         revision=revision,
@@ -39,5 +41,14 @@ def loadModel(model_id: str, load=True):
         # Work around https://github.com/huggingface/diffusers/issues/1246
         # low_cpu_mem_usage=False if USE_DREAMBOOTH else True,
     )
+    from_pretrained = round((time.time() - from_pretrained) * 1000)
+
+    if load:
+        to_gpu = time.time()
+        model.to("cuda")
+        to_gpu = round((time.time() - to_gpu) * 1000)
+        print(f"Loaded from disk in {from_pretrained} ms, to gpu in {to_gpu} ms")
+    else:
+        print(f"Downloaded in {from_pretrained} ms")
 
     return model.to("cuda") if load else None
