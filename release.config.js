0d6a754fb3a47b75907ff01007969893c322e225
Gadi Cohen <dragon@wastelands.net>
2023-01-23 17:09:43 +0200

    chore(release): branches: remove 'split', add 'main'

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 // https://semantic-release.gitbook.io/semantic-release/support/faq#can-i-use-semantic-release-to-publish-non-javascript-packages
 module.exports = {
-  "branches": ["split"],
+  "branches": ["main"],
   "plugins": [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
