2066c446ba058209d1f594a46a8af0188e6e82fa
Gadi Cohen <dragon@wastelands.net>
2023-03-04 11:36:28 +0000

    feat(HTTPStorage): store filename from content-disposition

--- api/utils/storage/HTTPStorage.py
+++ api/utils/storage/HTTPStorage.py
@@ -25,6 +25,10 @@ class HTTPStorage(BaseStorage):
         print(f"Downloading {self.url} to {fname}...")
         resp = requests.get(self.url, stream=True)
         total = int(resp.headers.get("content-length", 0))
+        content_disposition = resp.headers["content-disposition"]
+        filename_search = re.search('filename="(.+)"', content_disposition)
+        if filename_search:
+            self.filename = filename_search.group(1)
         # Can also replace 'file' with a io.BytesIO object
         with open(fname, "wb") as file, tqdm(
             desc="Downloading",
